f130aacc5f2deb70b86e08af8d7008ef113afce7
Laboratorios <laboratorios@unlmlab.edu.ar>
2022-08-17 10:17:31 -0300

    nuevoMeteodo

--- src/Dominio/Circulo.java
+++ src/Dominio/Circulo.java
@@ -2,4 +2,15 @@ package Dominio;
 
 public class Circulo {
 	private Double radio=0.0;
+
+	public void x (){
+		
+	}
+	public Double getRadio() {
+		return radio;
+	}
+
+	public void setRadio(Double radio) {
+		this.radio = radio;
+	}
 }
